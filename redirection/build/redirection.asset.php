70d00a774cd60b86dbe79359d68dc7e21b6ac6b3
Justin Gardner <justin@rhynorater.com>
2026-02-15 06:02:00 -0500

    5.7

--- redirection/build/redirection.asset.php
+++ redirection/build/redirection.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => 'fb33636f5672192cf3a8');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => '4d23cc70910fcfe0b242');
