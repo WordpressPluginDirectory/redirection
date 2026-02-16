be55c5a0cdba223d1e45108bc1d91f644e590964
Justin Gardner <justin@rhynorater.com>
2026-02-16 06:01:33 -0500

    5.7.1

--- redirection/build/redirection-version.php
+++ redirection/build/redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '5.7.0' );
-define( 'REDIRECTION_BUILD', 'e890d871866531d34315ea0473d27c98' );
-define( 'REDIRECTION_MIN_WP', '6.4' );
+define( 'REDIRECTION_VERSION', '5.7.1' );
+define( 'REDIRECTION_BUILD', 'acb297bb964b0170071ec6ce6ad3cf8a' );
+define( 'REDIRECTION_MIN_WP', '6.5' );
--- redirection/build/redirection.asset.php
+++ redirection/build/redirection.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => '4d23cc70910fcfe0b242');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => 'acbda9b47e79ed486d45');
